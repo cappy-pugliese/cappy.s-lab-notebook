f6e8389723894c207f8bef5d9b95e20cb7571859
Cappy <cappy.pugliese@gmail.com>
2024-09-18 14:01:00 -0400

    Renamed test.R and added info
    renamed test.R to 9-18-24_weekly_update and started to add some information. Want to try to figure out how to use markdown in R

---
+++ Weekly updates/9-18-24_weekly_update.R
@@ -0,0 +1,10 @@
+library(markdown)
+#Last week
+finished reading most of the Forsythe 2021 paper
+finished my individual development plan
+practiced more with using github
+learned about the cluster and started the sign up process
+This week I plan to:
+finish the rest of the Forsythe paper
+read some more WNS papers
+finish signing up for the cluster
